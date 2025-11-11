2ffbd653aa8967a4aae1ee5433ddb2d863dd2f2f
mugish.beldar <mugish.beldar@rapidops.com>
2025-11-11 16:28:28 +0530

    Feat | Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import './App.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
         <Contact />
       </main>
       <Footer />
+      <Analytics />
     </>
   )
 }
